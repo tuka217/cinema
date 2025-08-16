b8a7089534ad2ccacfaab16f94f2a0dfca04b3ed
Anna Walasek <anna.walasek@ttpsc.com>
2025-08-16 17:58:08 +0200

    added slide show

---
+++ src/components/main/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './Main.scss';
+import MainContent from '../content/main-content/MainContent';
+
+const Main = () => {
+  return (
+    <div className="main">
+      <MainContent />
+    </div>
+  );
+};
+
+export default Main;
